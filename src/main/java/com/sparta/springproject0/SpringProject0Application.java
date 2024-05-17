a481c9515e19d472e7c6a74d891e5b14e1fe7972
lhu <lhu0505@naver.com>
2024-05-17 13:57:39 +0900

    init

---
+++ src/main/java/com/sparta/springproject0/SpringProject0Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springproject0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProject0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springproject0/SpringProject0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springproject0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
